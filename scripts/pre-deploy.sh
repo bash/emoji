8aca0357f6e3ad683ea1ecc69d963bced8134620
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-10-05 12:11:44 +0200

    Copy files to /public before deployment

---
+++ scripts/pre-deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+files=(
+    build/js/emoji.js
+    build/css/emoji.css
+    index.html
+)
+
+public_dir=public
+
+for file in ${FILES[@]}
+do
+    mkdir -p "$public_dir/$(dirname "$file")"
+    cp -R "$file" "$public_dir/$file"
+done
